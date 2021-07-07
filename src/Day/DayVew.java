0245f9f3ae5da3c80afedb8da6444ea7cdaf6135
Nadejda <mezz@74.ru>
2021-07-07 19:26:17 +0500

    JComboBox пробуем добавилять, в разработке

--- src/Day/DayVew.java
+++ src/Day/DayVew.java
@@ -5,20 +5,46 @@ import java.awt.*;
 
 public class DayVew extends JPanel{
 
-    private JPanel panelDay = new JPanel();
-    private JLabel namber1 = new JLabel("1");
-    private JLabel namber2 = new JLabel("2");
-    private JLabel namber3 = new JLabel("3");
-    private JTextArea class1 = new JTextArea("Выберите вид занятий");
-    private JTextArea class2 = new JTextArea("Выберите вид занятий");
-    private JTextArea class3 = new JTextArea("Выберите вид занятий");
+
     //   private JLabel dayTitle = new JLabel("Расписание занятий на день");
     //   private JTextArea data = new JTextArea("День недели/дата занятия");
 //    private JButton toWeekButton = new JButton("На главную страницу Расписание на неделю");
   //  private final Day day;
 
+//    private static void choosingClasses(String, class) {
+//        JComboBox<String> class = new JComboBox<String>();
+//        this.class = class;
+//        class.addItem("Рисование");
+//        class.addItem("Лепка");
+//        class.addItem("Физическое виспитание");
+//        class.addItem("Ознакомление с окружающей стредой");
+//        class.addItem("Вырезание");
+//        class.addItem("Разукрашивание");
+//        add(class);
+//        return class;
+//    }
+
+
     public DayVew(int n) {
   //      this.day = day;
+
+         JPanel panelDay = new JPanel();
+         JLabel namber1 = new JLabel("Занятие 1");
+         JLabel namber2 = new JLabel("Занятие 2");
+         JLabel namber3 = new JLabel("Занятие 3");
+         JComboBox<String> class1 = new JComboBox<String>();
+  //      choosingClasses(class1);
+
+         JComboBox<String> class2 = new JComboBox<String>();
+        class2.addItem("Рисование");
+        class2.addItem("Лепка");
+        class2.addItem("Физическое виспитание");
+        class2.addItem("Ознакомление с окружающей стредой");
+        class2.addItem("Вырезание");
+        class2.addItem("Разукрашивание");
+        add(class1);
+
+         JTextArea class3 = new JTextArea("Выберите вид занятий");
 
         this.setBorder(BorderFactory.createTitledBorder("panel" + n));
         GroupLayout layout = new GroupLayout(this);
@@ -57,7 +83,9 @@ public class DayVew extends JPanel{
         jframe.setLayout(new BoxLayout(jframe.getContentPane(), BoxLayout.Y_AXIS));
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.add(new DayVew(1));
-        jframe.add(Box.createVerticalGlue());
+        jframe.add(new DayVew(2));
+        jframe.add(new DayVew(3));
+       // jframe.add(Box.createVerticalGlue());
         jframe.pack();
         jframe.setLocationRelativeTo(null);
         jframe.setVisible(true);
